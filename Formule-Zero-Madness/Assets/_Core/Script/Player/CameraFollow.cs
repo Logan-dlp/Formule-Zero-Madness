d2fc7766f7bd04dec4cc8b65f8e21e5b726a610f
Simple <pix.59700@gmail.com>
2023-01-05 14:19:05 +0100

    racing

--- Formule-Zero-Madness/Assets/_Core/Script/Player/CameraFollow.cs
+++ Formule-Zero-Madness/Assets/_Core/Script/Player/CameraFollow.cs
@@ -3,15 +3,15 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     #region Settings
-    public float Smoothing;
-    public Transform Player;
+    [SerializeField] float smoothing;
+    [SerializeField] Transform player;
     #endregion
     #region Meths Unity
     private void FixedUpdate()
     {
-        transform.position = Vector3.Lerp(transform.position, Player.position, Smoothing);
+        transform.position = Vector3.Lerp(transform.position, player.position, smoothing);
 
-        transform.rotation = Quaternion.Slerp(transform.rotation, Player.rotation, Smoothing);
+        transform.rotation = Quaternion.Slerp(transform.rotation, player.rotation, smoothing);
         transform.rotation = Quaternion.Euler(new Vector3(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z));
     }
     #endregion
